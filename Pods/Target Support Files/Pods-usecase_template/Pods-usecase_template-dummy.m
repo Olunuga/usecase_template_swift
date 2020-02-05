f2d318b5c943662c36abb3b1843cefe476a507d6
Mayowa Olunuga <backupbox86@gmail.com>
2020-02-05 10:58:34 +0100

    Removing all files in .gitignore

--- Pods/Target Support Files/Pods-usecase_template/Pods-usecase_template-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_usecase_template : NSObject
-@end
-@implementation PodsDummy_Pods_usecase_template
-@end
--- Pods/Target Support Files/Pods-usecase_templateTests/Pods-usecase_templateTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_usecase_templateTests : NSObject
-@end
-@implementation PodsDummy_Pods_usecase_templateTests
-@end
